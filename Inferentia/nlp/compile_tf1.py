9ab9f9fe6ded1563f69e8d98a1683427aa74c96e
manchann <chl8273@naver.com>
2022-12-28 16:39:49 +0900

    use bert pretrained model

--- Inferentia/nlp/compile_tf1.py
+++ Inferentia/nlp/compile_tf1.py
@@ -8,12 +8,13 @@ import json
 import numpy as np
 import time
 
-model_name = model_type = 'bert-base-uncased'
-
-model = TFBertForSequenceClassification.from_pretrained(model_name, num_labels=2)
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=2e-5, epsilon=1e-08),
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=["acc"])
-original_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+model_type ='bert_base'
+# model_name = model_type = 'bert-base-uncased'
+#
+# model = TFBertForSequenceClassification.from_pretrained(model_name, num_labels=2)
+# model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=2e-5, epsilon=1e-08),
+#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=["acc"])
+# original_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
 
 def compile_inf1_model(saved_model_dir, inf1_model_dir, batch_size=1, use_static_weights=False):
