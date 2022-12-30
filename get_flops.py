8db96f2528376527509af8c388aaececd9481cb5
manchann <chl8273@naver.com>
2022-12-30 15:03:09 +0900

    tf compat

--- get_flops.py
+++ get_flops.py
@@ -78,10 +78,10 @@ model_types = [
 
 def get_flops():
     run_meta = tf.compat.v1.RunMetadata()
-    opts = tf.profiler.ProfileOptionBuilder.float_operation()
+    opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
 
     # We use the Keras session graph in the call to the profiler.
-    flops = tf.profiler.profile(graph=K.get_session().graph,
+    flops = tf.compat.v1.profiler.profile(graph=K.get_session().graph,
                                 run_meta=run_meta, cmd='op', options=opts)
 
     return flops.total_float_ops  # Prints the "flops" of the model.
